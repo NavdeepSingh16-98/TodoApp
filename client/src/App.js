c49fc4cf3234e2c2ff3a09ff940865b1f49fe260
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2022-12-24 22:53:02 +0530

    todo app started

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import SignUp from './components/SignUp'
+import NoPage from './components/NoPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+    {/* Welcome to App */}
+
+
+    <Routes>
+    <Route path="/" >
+          <Route index element={<SignUp />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} /> */}
+          <Route path="*" element={<NoPage />} />
+        </Route>
+    </Routes>
+    {/* <SignUp /> */}
+
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/NoPage.js
@@ -0,0 +1,11 @@
+function NoPage(){
+
+
+    return(
+        <>
+        <h1 className="text-red-400">Page Not Found,URL does not exist</h1>
+        </>
+    )
+}
+
+export default NoPage
