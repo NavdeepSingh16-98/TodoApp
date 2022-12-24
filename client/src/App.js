d5df5f56cbaafde4b0e41e39429cbbbb425de93d
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2022-12-24 23:34:32 +0530

    login and signup page improved

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import SignUp from './components/SignUp'
 import NoPage from './components/NoPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Routes>
     <Route path="/" >
           <Route index element={<SignUp />} />
+          <Route path="login" element={<Login />} />
           {/* <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} /> */}
           <Route path="*" element={<NoPage />} />
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,10 @@
 import { NavLink } from "react-router-dom"
-
-function SignUp(){
-
+function Login(){
 
     return(
+
         <>
-        <section class="h-screen">
+       <section class="h-screen">
   <div class="px-6 h-full text-gray-800">
     <div
       class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
@@ -21,20 +20,9 @@ function SignUp(){
       </div>
       <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
         <form>
-        
-
        
 
           {/* <!-- Email input --> */}
-          <div class="mb-6">
-            <input
-              type="text"
-              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="exampleFormControlInput2"
-              placeholder="Name"
-            />
-          </div>
-
           <div class="mb-6">
             <input
               type="text"
@@ -44,7 +32,7 @@ function SignUp(){
             />
           </div>
 
-              {/* <!-- Password input --> */}
+          {/* <!-- Password input --> */}
           <div class="mb-6">
             <input
               type="password"
@@ -55,8 +43,17 @@ function SignUp(){
           </div>
 
           <div class="flex justify-between items-center mb-6">
-          
-           
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                id="exampleCheck2"
+              />
+              <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
+                >Remember me
+                </label>
+            </div>
+            <a href="#!" class="text-gray-800">Forgot password?</a>
           </div>
 
           <div class="text-center lg:text-left">
@@ -64,15 +61,14 @@ function SignUp(){
               type="button"
               class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
-              Register
+              Login
             </button>
             <p class="text-sm font-semibold mt-2 pt-1 mb-0">
-              Already have an account?
-              <NavLink
-                to="login"
-                class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out" >
-                    Login In
-                </NavLink >
+              Don't have an account?
+              
+                <NavLink to="/"
+                class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                >Register</NavLink>
             </p>
           </div>
         </form>
@@ -84,4 +80,4 @@ function SignUp(){
     )
 }
 
-export default SignUp
+export default Login
