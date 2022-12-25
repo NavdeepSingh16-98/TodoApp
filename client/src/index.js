32177f4ac4e030b7524f1b88613c06182b149f93
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2022-12-26 01:15:25 +0530

    dashboard improved

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '@fortawesome/fontawesome-free'
+//import '@fortawesome/fontawesome-free'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
